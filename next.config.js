8fc8bc5a290ce273de200affd21fc904b3398bcb
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2023-05-17 22:37:06 +0100

    serverless

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images : {
-    domains : ["s.gravatar.com"]
-  }
+  images: {
+    domains: ["s.gravatar.com"],
+  },
+  serverless: {
+    // Increase timeout for the serverless function
+    timeout: 30, // timeout duration in seconds
+    // Other serverless function configurations
+    // ...
+  },
 }
 
 module.exports = nextConfig
