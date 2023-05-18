442858a7c0960465a346d2f7c3e11af59048b4a2
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2023-05-18 13:49:38 +0100

    modify content

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -34,9 +34,9 @@ export default withApiAuthRequired(async function handler(req, res) {
     },
     {
       role: "user",
-      content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}. 
+      content: `Write a brief SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}. 
       The response should be formatted in SEO-friendly HTML, 
-      limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, i, ul, li, ol.`,
+      limited to the following HTML tags: p, h1, h2 , ul, li.`,
     },
   ]
 
